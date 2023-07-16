331614824416895394f2b132b1bcf814903a76ec
aleks <achudars@gmail.com>
2023-07-16 17:10:32 +0100

    feat: add React plugin (qwikReact) and Material UI with Masonry grid

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { qwikVite } from '@builder.io/qwik/optimizer'
+import { qwikReact } from '@builder.io/qwik-react/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
     qwikVite({
       csr: true,
     }),
+    qwikReact()
   ],
 })
