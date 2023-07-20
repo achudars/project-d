ac2856c68ace200f440930d3c4cffcd81dec1157
aleks <achudars@gmail.com>
2023-07-20 15:51:58 +0100

    feat: run prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import { qwikVite } from '@builder.io/qwik/optimizer'
-import { qwikReact } from '@builder.io/qwik-react/vite';
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikReact } from "@builder.io/qwik-react/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,6 @@ export default defineConfig({
     qwikVite({
       csr: true,
     }),
-    qwikReact()
+    qwikReact(),
   ],
-})
+});
