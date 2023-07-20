cf1cc1841bcb59c8bbb487a6a875a4c5bef567d8
aleks <achudars@gmail.com>
2023-07-20 15:49:41 +0100

    feat: add .eslintrc.cjs

--- src/Items.tsx
+++ src/Items.tsx
@@ -1,31 +1,32 @@
 /** @jsxImportSource react */
-import {qwikify$} from '@builder.io/qwik-react';
+import { qwikify$ } from "@builder.io/qwik-react";
 import Box from "@mui/material/Box";
 import Masonry from "@mui/lab/Masonry";
 import items from "./data/data.json";
 
 // Create React component standard way
 export const Items = qwikify$(() => {
-    return (<Box sx={{width: 500, minHeight: 800}}>
-        <Masonry
-            columns={3} spacing={2}>
-            {items.map((item, index) => (
-                <div key={index}>
-                    <p>{index + 1}</p>
-                    <img
-                        src={`${item.img}?w=162&auto=format`}
-                        srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-                        alt={item.title}
-                        loading="lazy"
-                        style={{
-                            borderBottomLeftRadius: 4,
-                            borderBottomRightRadius: 4,
-                            display: 'block',
-                            width: '100%',
-                        }}
-                    />
-                </div>
-            ))}
-        </Masonry>
-    </Box>);
+  return (
+    <Box sx={{ width: 500, minHeight: 800 }}>
+      <Masonry columns={3} spacing={2}>
+        {items.map((item, index) => (
+          <div key={index}>
+            <p>{index + 1}</p>
+            <img
+              src={`${item.img}?w=162&auto=format`}
+              srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+              style={{
+                borderBottomLeftRadius: 4,
+                borderBottomRightRadius: 4,
+                display: "block",
+                width: "100%",
+              }}
+            />
+          </div>
+        ))}
+      </Masonry>
+    </Box>
+  );
 });
--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,7 @@
-import {component$} from '@builder.io/qwik'
-import './app.css'
-import {Items} from "./Items.tsx";
+import { component$ } from "@builder.io/qwik";
+import "./app.css";
+import { Items } from "./Items.tsx";
 
 export const App = component$(() => {
-
-    return <Items/>
-})
+  return <Items />;
+});
